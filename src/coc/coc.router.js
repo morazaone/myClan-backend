e135b45c61395c641d16f17e04121dc61bfb8302
juan <morazaone@gmail.com>
2022-10-10 16:41:39 -0500

    read clan tag from .env

--- src/coc/coc.router.js
+++ src/coc/coc.router.js
@@ -3,14 +3,13 @@
  */
 
  const express = require("express");
- const { getPublicMessage } = require("./coc.service");
  const axios = require("axios");
  const TOKEN = process.env.TOKEN;
-
+const CLAN_TAG = process.env.CLAN_TAG;
   /**
   * Router Definition
   */
- 
+   
  const cocRouter = express.Router();
  
  /**
@@ -28,14 +27,14 @@
   let resp;
   //json format 
   try {
-    resp = await axios.get('https://api.clashofclans.com/v1/clans/%238RP8LRG', 
+    resp = await axios.get('https://api.clashofclans.com/v1/clans/%23'+ CLAN_TAG, 
     { 
       headers: {
           Authorization: `Bearer ` + TOKEN
         }
       }
     );
-    //  console.log(resp.data);
+    // console.log(resp.data);
   } catch (error) {
     console.log('something went wrong with updateGithubFile()', error);
     res.status(400).send({
@@ -45,9 +44,44 @@
     });
   }
    console.log(res.data)
+   console
    res.status(200).send({
     statusCode:200,
-    body:resp.data
+    body:resp.data,
+    member:resp.data[0]
+    
+  });
+});
+
+cocRouter.get("/getMemberInfo", async (req, res) => {
+  let resp;
+  const memberTag = req.query.memberTag.substring(1);
+  console.log(memberTag)
+  // json format 
+  try {
+    resp = await axios.get('https://api.clashofclans.com/v1/players/%23'+memberTag, 
+    { 
+      headers: {
+          Authorization: `Bearer ` + TOKEN
+        }
+      }
+    );
+    // console.log(resp.data);
+  } catch (error) {
+    console.log('something went wrong with updateGithubFile()', error);
+    res.status(400).send({
+      statusCode:200,
+      body:error
+
+    });
+  }
+   console.log(res.data)
+   console
+   res.status(200).send({
+    statusCode:200,
+    body:resp.data,
+    member:resp.data[0]
+    
   });
 });
 
